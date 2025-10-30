8669acec2c6daf09317ca269b8691434a62209eb
MagnusLucas <lukasz.kisielewski@student.uj.edu.pl>
2025-10-30 14:14:01 +0100

    Somewhat improved velocity based rotation

--- components/velocity_based_rotation.gd
+++ components/velocity_based_rotation.gd
@@ -4,8 +4,9 @@ extends Node
 @export var enabled: bool = true : set = _set_enabled
 @export var target: Node2D
 @export_range(0.25, 1.5) var lerp_seconds := 0.4
-@export var rotation_multiplier := 120
-@export var x_velocity_threshold := 3.0
+@export var max_rotation_degrees := 50
+@export var x_velocity_lower_threshold := 1.0
+@export var x_velocity_upper_threshold := 3.0
 
 var last_position: Vector2
 var velocity: Vector2
@@ -13,20 +14,25 @@ var angle: float
 var progress: float
 var time_elapsed := 0.0
 
+func easeInOutQuad(x: float) -> float:
+	return 2 * x * x if x < 0.5 else 1 - pow(-2 * x + 2, 2) / 2;
 
-func _process(delta: float) -> void:
+func _physics_process(delta: float) -> void:
 	if not enabled or not target:
 		return
 	velocity = target.global_position - last_position
 	last_position = target.global_position
 	progress = time_elapsed/lerp_seconds
 	
-	if abs(velocity.x) > x_velocity_threshold:
-		angle = velocity.normalized().x * rotation_multiplier * delta
+	if abs(velocity.x) > x_velocity_upper_threshold:
+		angle = velocity.normalized().x * deg_to_rad(max_rotation_degrees)
+	elif angle != 0 and abs(velocity.x) > x_velocity_lower_threshold:
+		angle = velocity.normalized().x * deg_to_rad(max_rotation_degrees)
+		time_elapsed = 0.3
 	else:
 		angle = 0.0
 	
-	target.rotation = lerp_angle(target.rotation, angle, progress)
+	target.rotation = lerp_angle(target.rotation, angle, easeInOutQuad(progress))
 	time_elapsed += delta
 	
 	if progress > 1.0:
