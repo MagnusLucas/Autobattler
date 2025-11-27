b73a908dec5d13ab5a5f0bc44f4c4f210016605a
MagnusLucas <lukasz.kisielewski@student.uj.edu.pl>
2025-11-27 01:42:44 +0100

    Added battle units script for visuals

--- data/units/unit_stats.gd
+++ data/units/unit_stats.gd
@@ -2,6 +2,10 @@ class_name UnitStats
 extends Resource
 
 enum Rarity {COMMON, UNCOMMON, RARE, EPIC, LEGENDARY}
+enum Team {
+	PLAYER,
+	ENEMY
+}
 
 const RARITY_COLORS = {
 	Rarity.COMMON: Color.WEB_GRAY,
@@ -9,6 +13,11 @@ const RARITY_COLORS = {
 	Rarity.RARE: Color.DARK_BLUE,
 	Rarity.EPIC: Color.DARK_VIOLET,
 	Rarity.LEGENDARY: Color.GOLD,
+}
+
+const TEAM_SPRITESHEET := {
+	Team.PLAYER : preload("uid://cx1ivjobggp8n"),
+	Team.ENEMY : preload("uid://ct4xk88g67xlv")
 }
 
 @export var name : String
@@ -23,6 +32,8 @@ const RARITY_COLORS = {
 @export_category("Visuals")
 @export var skin_coordinates: Vector2i
 
+@export_category("Battle")
+@export var team: Team
 
 func _set_tier(value: int) -> void:
 	tier = value
---
+++ scenes/battle_unit/battle_unit.gd
@@ -0,0 +1,25 @@
+class_name BattleUnit
+extends Area2D
+
+@export var stats: UnitStats: set = _set_stats
+
+@onready var skin: PackedSprite2D = $Skin
+@onready var health_bar: ProgressBar = $HealthBar
+@onready var mana_bar: ProgressBar = $ManaBar
+@onready var tier_icon: TierIcon = $TierIcon
+@onready var animation_player: AnimationPlayer = $AnimationPlayer
+
+
+func _set_stats(value: UnitStats) -> void:
+	stats = value
+	
+	if not stats or not is_node_ready():
+		return
+	
+	stats = stats.duplicate()
+	collision_layer = stats.team + 1
+	
+	skin.texture = UnitStats.TEAM_SPRITESHEET[stats.team]
+	skin.coordinates = stats.skin_coordinates
+	skin.flip_h = stats.team == UnitStats.Team.PLAYER
+	tier_icon.unit_stats = stats
